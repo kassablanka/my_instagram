8a5c1f69a4399d1e4e14a765a5eb601480b45aba
Keweegen <keweegen@gmail.com>
2019-05-26 23:59:51 +0600

    React Course, day 2. Home Work :3

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react'
-import Post from './Post'
+import User from './User'
+import InstaService from '../services/instaservice'
+import ErrorMessage from './ErrorMessage'
 
 export default class Posts extends Component {
+
+    InstaService = new InstaService()
+
+    state = {
+        posts: [],
+        error: false
+    }
+
+    componentDidMount() {
+        this.updatePosts()
+    }
+
+    updatePosts() {
+        this.InstaService.getAllPosts()
+        .then(this.onPostsLoaded)
+        .catch(this.onError)
+    }
+
+    onPostsLoaded = (posts) => {
+        this.setState({
+            posts,
+            error: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map(item => {
+            const { name, altname, photo, src, alt, descr, id } = item
+
+            return (
+                <div className="post" key={ id }>
+                    <User
+                        src={ photo }
+                        alt={ altname }
+                        name={ name }
+                        min
+                    />                
+                    <img src={ src } alt={ alt } />
+                    <div className="post__name">{ name}</div>
+                    <div className="post__descr">{ descr }</div>
+                </div>
+            )
+        })
+    }
+
     render() {
+
+        const { error, posts } = this.state
+
+        if(error) {
+            return <ErrorMessage />
+        }
+
+        const items = this.renderItems(posts)
+
         return(
-            <div className="left">
-                <Post alt="spider" src="https://images.pexels.com/photos/2317423/pexels-photo-2317423.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"  />
-                <Post alt="city" src="https://images.pexels.com/photos/2338113/pexels-photo-2338113.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"  />
-            </div>
+            <div className="left">{ items }</div>
         )
     }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,43 +1,79 @@
 import React, { Component } from 'react'
 import User from './User'
+import InstaService from '../services/instaservice'
+import ErrorMessage from './ErrorMessage'
 
-export default function Users() {
-    return(
-        <div className="right">
 
-            <User
-                src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt="Woman"
-                name="Keweegen"
-            /> 
+export default class Users extends Component {
 
-            <div className="users__block">
+    InstaService = new InstaService()
+
+    state = {
+        users: [],
+        error: false
+    }
+
+    componentDidMount() {
+        this.updateUsers()
+    }
+
+    updateUsers() {
+        this.InstaService.getAllPosts()
+        .then(this.onUsersLoaded)
+        .catch(this.onError)
+    }
+
+    onUsersLoaded = (users) => {
+        this.setState({
+            users,
+            error: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map(item => {
+            const { name, altname, photo, id } = item
+
+            return (
+                <a href="#" className="user min" key={id}>
+                    <img src={photo} altname={altname} />
+                    <div>{name}</div>
+                </a>
+            )
+        })
+    }
+
+    render() {
+
+        const {error, users} = this.state
+
+        if(error) {
+            return <ErrorMessage/>
+        }
+
+        const items = this.renderItems(users)
+        
+        return(
+            <div className="right">
+
                 <User
                     src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                     alt="Woman"
                     name="Keweegen"
-                    min
                 /> 
-                <User
-                    src="https://images.pexels.com/photos/1917785/pexels-photo-1917785.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    alt="Woman"
-                    name="name"
-                    min
-                /> 
-                <User
-                    src="https://images.pexels.com/photos/2316911/pexels-photo-2316911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                    alt="Woman"
-                    name="infiniti"
-                    min
-                /> 
-                <User
-                    src="https://images.pexels.com/photos/1779536/pexels-photo-1779536.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                    alt="Woman"
-                    name="linvitks"
-                    min
-                /> 
-            </div>
 
-        </div>
-    )
+                <div className="users__block">
+                    { items }
+                </div>
+
+            </div>
+        )
+
+    }
 }
